3541bea887f2d270154328836d01eff7143eec06
Nikita Pelenskyi <67456353+npelenskii@users.noreply.github.com>
2022-09-10 15:25:42 +0300

    v1

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("form"),
         inputs = document.getElementsByClassName("input"),
-        firstName = document.getElementById("firstName"),
-        firstNameLabel = document.getElementById("firstNameIcon"),
-        firstNameError = document.getElementById("firstNameError"),
-        lastName = document.getElementById("lastName"),
-        lastNameLabel = document.getElementById("lastNameIcon"),
-        lastNameError = document.getElementById("lastNameError"),
-        email = document.getElementById("email"),
-        emailLabel = document.getElementById("emailIcon"),
-        emailError = document.getElementById("emailError"),
-        password = document.getElementById("password"),
-        passwordLabel = document.getElementById("passwordIcon"),
-        passwordError = document.getElementById("passwordError");
+        labels = document.getElementsByClassName("label"),
+        errorText = document.getElementsByClassName("input__error");
     let isValid = true;
 
     const validateEmail = (email) => {
@@ -23,66 +13,61 @@ window.addEventListener("DOMContentLoaded", () => {
             );
     };
 
-    console.log(inputs);
-
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        if (firstName.value === "" || firstName.value == null) {
-            isValid = false;
-            firstName.classList.add("input__failed");
-            firstName.placeholder = "";
-            firstNameLabel.classList.add("error__label");
-            firstNameError.classList.add("show");
-        }
-
-        if (lastName.value === "" || lastName.value == null) {
-            isValid = false;
-            lastName.classList.add("input__failed");
-            lastName.placeholder = "";
-            lastNameLabel.classList.add("error__label");
-            lastNameError.classList.add("show");
-        }
-
-        if (validateEmail(email.value) || email.value == null) {
-            isValid = false;
-            email.classList.add("input__failed");
-            email.value = "";
-            email.placeholder = "email@example/com";
-            emailLabel.classList.add("error__label");
-            emailError.classList.add("show");
-        }
-
-        if (password.value === "" || password.value == null) {
-            isValid = false;
-            password.classList.add("input__failed");
-            password.placeholder = "";
-            passwordLabel.classList.add("error__label");
-            passwordError.classList.add("show");
-        }
-    });
-
-    for (var i = 0; i < inputs.length; i++) {
-        inputs[i].addEventListener("focus", () => {
-            if (!isValid) {
-                isValid = true;
-                firstName.classList.remove("input__failed");
-                firstName.placeholder = "First Name";
-                firstNameLabel.classList.remove("error__label");
-                firstNameError.classList.remove("show");
-                lastName.classList.remove("input__failed");
-                lastName.placeholder = "Last Name";
-                lastNameLabel.classList.remove("error__label");
-                lastNameError.classList.remove("show");
-                email.classList.remove("input__failed");
-                email.placeholder = "Email Address";
-                emailLabel.classList.remove("error__label");
-                emailError.classList.remove("show");
-                password.classList.remove("input__failed");
-                password.placeholder = "Password";
-                passwordLabel.classList.remove("error__label");
-                passwordError.classList.remove("show");
+        Array.prototype.forEach.call(inputs, function (input, index) {
+            if (input.name !== "email") {
+                if (input.value === "" || input.value == null) {
+                    isValid = false;
+                    input.classList.add("input__failed");
+                    input.placeholder = "";
+                    labels[index].classList.add("error__label");
+                    errorText[index].classList.add("show");
+                }
+            } else if (input.name === "email") {
+                if (validateEmail(email.value) || email.value == null) {
+                    input.classList.add("input__failed");
+                    input.value = "";
+                    input.placeholder = "email@example/com";
+                    labels[index].classList.add("error__label");
+                    errorText[index].classList.add("show");
+                }
+            } else {
+                if (input.value === "" || input.value == null) {
+                    isValid = false;
+                    input.classList.add("input__failed");
+                    input.placeholder = "";
+                    labels[index].classList.add("error__label");
+                    errorText[index].classList.add("show");
+                }
             }
         });
-    }
+    });
+
+    Array.prototype.forEach.call(inputs, (input) => {
+        input.addEventListener("focus", () => {
+            if (!isValid) {
+                Array.prototype.forEach.call(inputs, (input, index) => {
+                    input.classList.remove("input__failed");
+                    labels[index].classList.remove("error__label");
+                    errorText[index].classList.remove("show");
+
+                    if (input.name === "firstName") {
+                        input.placeholder = "First Name";
+                    }
+                    if (input.name === "lastName") {
+                        input.placeholder = "Last Name";
+                    }
+                    if (input.name === "email") {
+                        input.placeholder = "Email Address";
+                    }
+                    if (input.name === "password") {
+                        input.placeholder = "Password";
+                    }
+                });
+            }
+            isValid = true;
+        });
+    });
 });
